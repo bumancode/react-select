2ca3599a3394d013785c53c5ce09e63cac6562fd
Buman <serhii.buman@gmail.com>
2020-02-02 19:33:09 +0200

    First component added

---
+++ src/components/Select/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Select extends React.Component {
+  constructor(props){
+  	super(props)
+  }
+
+  render(){
+    return( 
+        <p>Select component</p>
+    	);
+  }
+}
+
+export default Select;
