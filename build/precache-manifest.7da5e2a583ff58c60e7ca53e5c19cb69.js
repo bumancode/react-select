6533ef82b56402c16c217f77dafbf24885596ac1
Buman <serhii.buman@gmail.com>
2020-02-04 16:30:27 +0200

    .

---
+++ build/precache-manifest.7da5e2a583ff58c60e7ca53e5c19cb69.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddf52d7bb8e856bb7f2c92c3c5fca851",
+    "url": "/react-select/index.html"
+  },
+  {
+    "revision": "de4300e7d1c9f57815c3",
+    "url": "/react-select/static/css/main.77bc7458.chunk.css"
+  },
+  {
+    "revision": "f6ccc12388b3c3eeba27",
+    "url": "/react-select/static/js/2.1cb1a40a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-select/static/js/2.1cb1a40a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de4300e7d1c9f57815c3",
+    "url": "/react-select/static/js/main.e73154c0.chunk.js"
+  },
+  {
+    "revision": "aa7f070dd38e3cd23044",
+    "url": "/react-select/static/js/runtime-main.dbc85e0d.js"
+  },
+  {
+    "revision": "15c2e706ae21975f980470f3d0c0a0a3",
+    "url": "/react-select/static/media/arrow.15c2e706.svg"
+  },
+  {
+    "revision": "43a75ea93e06e212b591139f8ebe4ce8",
+    "url": "/react-select/static/media/close.43a75ea9.svg"
+  }
+]);
